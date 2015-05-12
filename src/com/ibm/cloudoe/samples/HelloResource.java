a4a9ebcb9e66ed251aab806dc0563854a1dd367d
cgawron <christian.gawron@de.ibm.com>
2015-05-12 18:47:12 +0200

    Test HelloWorld

--- src/com/ibm/cloudoe/samples/HelloResource.java
+++ src/com/ibm/cloudoe/samples/HelloResource.java
@@ -11,13 +11,13 @@ public class HelloResource {
 	public String getInformation() {
 
 		// 'VCAP_APPLICATION' is in JSON format, it contains useful information about a deployed application
-		// String envApp = System.getenv("VCAP_APPLICATION");
+		String envApp = System.getenv("VCAP_APPLICATION");
 
 		// 'VCAP_SERVICES' contains all the credentials of services bound to this application.
-		// String envServices = System.getenv("VCAP_SERVICES");
-		// JSONObject sysEnv = new JSONObject(System.getenv());
+		String envServices = System.getenv("VCAP_SERVICES");
+		JSONObject sysEnv = new JSONObject(System.getenv());
 		
-		return "Hi World!";
+		return "Hi World!" + envApp;
 
 	}
 }
